f0ed0efa3edfaa6053d95d9dd59a7d5729daa905
AndriyMazuryk <andriy.mzrk@gmail.com>
2020-11-17 06:45:33 +0200

    Add irregular verbs page and routing.

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Home } from './pages'
-
-export default function App() {
-  return (
-      <Home />
-  )
-}
--- src/pages/IrregularVerbs.js
+++ src/pages/IrregularVerbs.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 import { Header, Footer } from '../components'
 
-export default function Home() {
+export default function IrregularVerbs() {
   return (
     <div className="wrapper">
       <Header />
       <main className="main">
         <div className="container">
-          <h1>Home</h1>
+          <h1>Irregular Verbs</h1>
           <nav className="nav">
-            <Link to="/irregular-verbs">Irregular Verbs</Link>
+            <Link to="/">Home</Link>
           </nav>
         </div>
       </main>
