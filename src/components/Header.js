e2bc860b886203cc1d9113d38e1cd08e313b186c
AndriyMazuryk <andriy.mzrk@gmail.com>
2020-11-17 21:59:35 +0200

    Turn a site logo in the header into a link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className="header">
       <div className="container">
-        <div className="header__logo">English Workouts</div>
+        <div className="header__logo">
+          <Link className="header__link" to="/">English Workouts</Link>
+        </div>
       </div>
     </header>
   )
